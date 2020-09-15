b325f6f3024c03fdcdd052798bcf03ca9afafec5
fidgett <fidget@fidget.ru>
2020-09-15 23:44:10 +0300

    First project

--- data/main/src/main.ts
+++ data/main/src/main.ts
@@ -3,12 +3,18 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { hmrBootstrap } from './hmr';
 
 if (environment.production) {
   enableProdMode();
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
-});
+const bootstrap = () => platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.log(err));
+
+if (environment.hmr) {
+  hmrBootstrap(module, bootstrap);
+} else {
+  bootstrap();
+}
